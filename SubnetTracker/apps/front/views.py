17c0879642e953f84b318b6f85754f87b019a087
Justin Crawford <Justasic@Gmail.com>
2014-01-09 10:16:03 -0800

    Initial project structure. Got some skeleton files ready
    - Added initial 'front' app which will likely be the only app. this will display the CIDR status lists.
    - Added jquery to the static folder
    - Setup settings.py with PostgreSQL as the database as well as additional settings that Django 1.6 doesn't seem to add anymore
    - Added initial skeleton templates which will be modified later

---
+++ SubnetTracker/apps/front/views.py
@@ -0,0 +1,11 @@
+__author__ = 'justasic'
+
+from django.shortcuts import render, render_to_response
+from django.template.context import RequestContext
+
+def index(request):
+	ctx = RequestContext(request, {
+		'posts': [],
+		})
+
+	render_to_response('front/index.html', ctx)
